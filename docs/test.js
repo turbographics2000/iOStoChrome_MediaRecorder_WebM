7e7e869a81f6e330095143012b294bd6dcb6aa9b
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-10-16 19:31:59 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -1,26 +1,26 @@
 const apiKey = '96290d86-2e8a-490a-9e26-1be00036e7d6';
 
-const peer = new Peer({key: apiKey});
+const peer = new Peer({ key: apiKey });
 let mr = null;
 let recURL = null;
 
 peer.on('open', _ => {
-  dispMyId.textContent = peer.id; 
-});  
+  dispMyId.textContent = peer.id;
+});
 peer.on('call', call => {
   call.on('stream', stream => {
     remotePreview.srcObject = stream;
   });
-  navigator.mediaDevices.getUserMedia({video:true}).then(stream => {
+  navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
     call.answer(stream);
   }).catch(e => {
     console.log('answer error');
   });
 });
 
-btnConnect.onclick = function() {
-  if(!txtConnectId.value.trim()) return;
-  navigator.mediaDevices.getUserMedia({video:true}).then(stream => {
+btnConnect.onclick = function () {
+  if (!txtConnectId.value.trim()) return;
+  navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
     const call = peer.call(txtConnectId.value.trim(), stream);
     call.on('stream', remoteStream => {
       remotePreview.srcObject = remoteStream;
@@ -29,19 +29,19 @@ btnConnect.onclick = function() {
     console.log('gum error', e);
   });
 };
-btnRecord.onclick = function() {
+btnRecord.onclick = function () {
   btnRecord.textContent = '録画中';
   btnRecord.disabled = true;
   mr = new MediaRecorder(remotePreview.srcObject, {
     mimeType: 'video/webm; codecs=vp8'
   });
   mr.start(10000);
-  mr.ondataavailable = function(evt) {
+  mr.ondataavailable = function (evt) {
     btnRecord.textContent = '録画';
     mr.stop();
-    recURL = URL.createObjectURL(new Blob([evt.data], 'video/webm'));
+    recURL = URL.createObjectURL(new Blob([evt.data], { type: 'video/webm' }));
   };
-  mr.onstop = function(evt) {
+  mr.onstop = function (evt) {
     mr = null;
     document.querySelectorAll('.download').forEach(elm => elm.remove());
     const download = document.createElement('a');
